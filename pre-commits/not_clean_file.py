75b07d24cdbb0876772861cad04b2f14cc44cdf0
florianpydde <florian.pydde@gmail.com>
2024-03-25 20:19:01 +0100

    Remove unused imports and commented out code

--- pre-commits/not_clean_file.py
+++ pre-commits/not_clean_file.py
@@ -1,6 +1,27 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas
+# import numpy as np
+
+# import pandas
+
+# import matplotlib.pyplot as plt
+
+# def bad_function(x, y):
+#     result = x + y
+#     return result
+
+
+# def another_bad_function():
+#     x = 10
+#     y = 20
+#     return x + y
+
+
+# def main():
+#     print(bad_function(10, 20))
+#     print(another_bad_function())
+
+
+# if __name__ == "__main__":
+#     main()
 
 
 def bad_function(x, y):
